fc2be622d7cd6cbda1c369f18dd35b7bd46ef151
Eliott CHEMINAL <eliott.cheminal@soitec.com>
2024-09-06 14:39:18 +0200

    Add  some improvment on Home page

--- Catalogue de produits/Pages/Index.cshtml.cs
+++ Catalogue de produits/Pages/Index.cshtml.cs
@@ -1,19 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Catalogue_de_produits.Models;
+using Catalogue_de_produits.Data;
+using Catalogue_de_produits.Pages.Produits;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Catalogue_de_produits.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Catalogue_de_produits.Data.Catalogue_de_produitsContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(ILogger<IndexModel> logger, Catalogue_de_produits.Data.Catalogue_de_produitsContext context)
         {
             _logger = logger;
+            _context = context;
+
         }
 
-        public void OnGet()
+        public IList<Produit> Produit { get; set; } = default!;
+
+
+        public async Task OnGetAsync()
         {
+            var produits = from p in _context.Produit
+                           select p;
+            Produit = await produits.ToListAsync();
 
         }
     }
